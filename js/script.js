6693c21c66e950b9dc9337dc28ad4d1acd258681
HaojieZhang123 <haojiezhang123@gmail.com>
2025-03-21 14:30:22 +0100

    [UPD] Palindroma: isPalindrome function created

--- js/script.js
+++ js/script.js
@@ -6,3 +6,11 @@
 let word = prompt('Inserisci una parola');
 
 // Funzione per capire se la parola inserita è palindroma
+function isPalindrome(string) {
+    let reverseWord = string.split('').reverse().join('');
+    if (word === reverseWord) {
+        console.log('La parola inserita è palindroma');
+    } else {
+        console.log('La parola inserita non è palindroma');
+    }
+}
