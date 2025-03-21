476b64e3cb9155f2de17b4eda82a9d0a7fe8476f
HaojieZhang123 <haojiezhang123@gmail.com>
2025-03-21 14:28:22 +0100

    [UPD] Palindroma: variable creation

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Palindroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// Chiedo all'utente di inserire una parola
+let word = prompt('Inserisci una parola');
+
+// Funzione per capire se la parola inserita è palindroma
